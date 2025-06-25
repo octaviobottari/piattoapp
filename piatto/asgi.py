c3044d57121f09713de7bf4fb718bbd35cb88d00
octaviobottari <80415861+octaviobottari@users.noreply.github.com>
2025-06-25 15:55:00 -0300

    Update asgi.py

--- piatto/asgi.py
+++ piatto/asgi.py
@@ -4,10 +4,11 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import core.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'piatto.settings')
-django.setup()
+django.setup()  # Move setup before imports
+
+import core.routing  # Import after django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),

780c569d4e839fce8b3f07e14c6ae04bfd92cd74
octaviobottari <80415861+octaviobottari@users.noreply.github.com>
2025-07-15 13:43:37 -0300

    Update storage_backends.py

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -1,10 +1,13 @@
+# core/storage_backends.py
+from django.conf import settings  # Add this import
 from storages.backends.s3boto3 import S3Boto3Storage
 import logging
 
 logger = logging.getLogger(__name__)
+
 logger.info(f"Usando almacenamiento: {settings.DEFAULT_FILE_STORAGE}")
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
-    file_overwrite = True
+    file_overwrite = False
     default_acl = 'public-read'

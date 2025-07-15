428fbaa6eff067c5f7792bc9ad2a967dd4b97623
octaviobottari <80415861+octaviobottari@users.noreply.github.com>
2025-07-15 00:27:31 -0300

    Update storage_backends.py

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -1,4 +1,8 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+import logging
+
+logger = logging.getLogger(__name__)
+logger.info(f"Usando almacenamiento: {settings.DEFAULT_FILE_STORAGE}")
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'

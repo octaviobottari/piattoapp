c0a7e62bac0b6d228c63967f667ce834c265e087
octaviobottari <80415861+octaviobottari@users.noreply.github.com>
2025-07-15 12:39:19 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -25,7 +25,7 @@ def get_restaurante_media_path(instance, filename):
         tipo = 'productos'
     elif tipo == 'restaurante':
         tipo = 'logos'
-    return f'media/{restaurante_username}/{tipo}/{filename}'
+    return f'{restaurante_username}/{tipo}/{filename}'  # Sin 'media/'
 
 class Restaurante(AbstractUser):
     ESTADO_CONTROL_CHOICES = [

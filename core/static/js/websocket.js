2e61666b603bec96c602f4242bdd6a5b1a8befbb
octaviobottari <octaviobottari7@gmail.com>
2025-08-11 21:42:04 -0300

    Update

--- core/static/js/websocket.js
+++ core/static/js/websocket.js
@@ -107,7 +107,7 @@ $(document).ready(function() {
                 cache: false,
                 timeout: 10000,
                 success: function(html) {
-                    $('.container').html($(html).find('.container, .container *'));
+                    $('.container').html($(html).find('.container').addBack().find('*').addBack());
                     $('.dropdown-toggle').dropdown();
                 },
                 error: function(xhr) {
